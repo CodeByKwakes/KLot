cb0c05f7b7aa9551c3cbaad2bc43b6a6c13d5c00
KwakesProject <kwakes@me.com>
2016-07-27 12:39:31 +0100

    move my two methods into a class called EnterUserNumbers

--- KLotVersion3/Program.cs
+++ KLotVersion3/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace KLotVersion3
 {
-
-    class Program
+    //int userInput;
+    
+    //public void IsNumberVaild(int userInput)
+    //{
+    //    bool userInputExists = userArray.Contains(userInput);
+    //    if (userInputExists)
+    //    {
+    //        Console.Write("\nYou Have already picked this number");
+    //    }
+    //    else
+    //    {
+    //        return;
+    //    }
+    //}
+    class EnterUserNumbers
     {
-        int userInput;
         int[] userArray = new int[6];
 
         public void InputLotteryNumbers()
@@ -18,6 +30,7 @@ namespace KLotVersion3
             {
                 Console.Write("\nPlease enter lottery number " + (i + 1) + " : ");
                 userArray[i] = int.Parse(Console.ReadLine());
+
             }
 
             //userInput = int.Parse(Console.ReadLine());
@@ -28,15 +41,14 @@ namespace KLotVersion3
             Console.Write("\nThe number you inputed was: {0}, {1}, {2}, {3}, {4}, {5}", userArray[0], userArray[1], userArray[2], userArray[3], userArray[4], userArray[5]);
             //Console.Write("\nThe number you inputed was: " + userInput);
         }
+    }
+    
 
-        public void UserLotteryTicket()
-        {
-
-        }
-
+    class Program
+    {
         static void Main(string[] args)
         {
-            Program kLot = new Program();
+            EnterUserNumbers kLot = new EnterUserNumbers();
             kLot.InputLotteryNumbers();
             Console.WriteLine("\n\n");
             kLot.ShowUserNumbers();
