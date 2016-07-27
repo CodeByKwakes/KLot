cf04786315b389259f981532558c2a94877b9773
KwakesProject <kwakes@me.com>
2016-07-27 12:04:31 +0100

    used for loop in InputLotteryNumbers to make sure i could enter 6 numbers in the userArray

--- KLotVersion3/Program.cs
+++ KLotVersion3/Program.cs
@@ -10,22 +10,35 @@ namespace KLotVersion3
     class Program
     {
         int userInput;
+        int[] userArray = new int[6];
 
         public void InputLotteryNumbers()
         {
-            Console.Write("\nPlease Enter a lottery number: ");
-            userInput = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("\nPlease Enter a lottery number: ");
+                userArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            //userInput = int.Parse(Console.ReadLine());
         }
 
         public void ShowUserNumbers()
         {
-            Console.Write("\nThe number you inputed was: " + userInput);
+            Console.Write("\nThe number you inputed was: {0}, {1}, {2}, {3}, {4}, {5}", userArray[0], userArray[1], userArray[2], userArray[3], userArray[4], userArray[5]);
+            //Console.Write("\nThe number you inputed was: " + userInput);
+        }
+
+        public void UserLotteryTicket()
+        {
+
         }
 
         static void Main(string[] args)
         {
             Program kLot = new Program();
             kLot.InputLotteryNumbers();
+            Console.WriteLine("\n\n");
             kLot.ShowUserNumbers();
             Console.ReadLine();
         }
