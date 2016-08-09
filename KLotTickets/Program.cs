fcc11c434387d5a01eababbbe7a8c378343c8a8b
KwakesProject <kwakes@me.com>
2016-08-09 17:17:10 +0100

    first working on this version in a single class. working on the ticket amount validation section

--- KLotTickets/Program.cs
+++ KLotTickets/Program.cs
@@ -8,15 +8,28 @@ namespace KLotTickets
 {
     class Program
     {
+        public int userMoney;
+        public int ticketPrice;
+        public int totalTicketAmount;
         public int[][] ticketNoArray;
         public int[] userArray;
         public int numberInput;
+        public bool status = false;
 
         static void Main(string[] args)
         {
             Program tkts = new Program();
+            Console.WriteLine("Please set the price of a lottery ticket:");
+            tkts.UserInput(out tkts.ticketPrice);
+            Console.WriteLine("How much money to you have:");
+            tkts.UserInput(out tkts.userMoney);
+            tkts.totalTicketAmount = tkts.AmountOfTickets();
+            Console.WriteLine($"You can buy {tkts.totalTicketAmount} tickets");
+
             Console.WriteLine("\nPlease enter the amount of lottery ticket you want to buy");
             tkts.UserInput(out tkts.numberInput);
+            tkts.IsTicketsNotValid();
+
             Console.WriteLine($"\nYou Entered: {tkts.numberInput} "); // testing
             tkts.SetTicketAmount();
             tkts.SetLotteyTickets();
@@ -30,6 +43,22 @@ namespace KLotTickets
             input = int.Parse(Console.ReadLine());
         }
 
+        public int AmountOfTickets()
+        {
+            totalTicketAmount = userMoney / ticketPrice;
+            return totalTicketAmount;
+        }
+
+        public bool IsTicketsNotValid()
+        {
+            bool status = false;
+            if (numberInput > totalTicketAmount)
+            {
+                Console.Write($"\nYou don not have enough money to buy {numberInput}\n");
+                status = true;
+            }
+            return status;
+        }
         public void SetTicketAmount()
         {
             ticketNoArray = new int[numberInput][];
